15faba686699edf935d3d7c16b92c2fb6e1a42d9
BMcGuffin <bryanmcguffin@gmail.com>
2018-04-17 11:54:24 -0700

    Generated new XML parsing handler. This class will scan an incoming XML file and use it to build a script object.

---
+++ Senior_Project_XML_Builder/src/Models/XML_ParseHandler.java
@@ -0,0 +1,50 @@
+/*
+ * This code was written as part of a senior project, for the
+ * Computer Science department at California Polytechnic State
+ * University, San Luis Obispo, in Winter/Spring 2018.
+ * The author of the project is Bryan McGuffin: bmcguffi@calpoly.edu
+ * Advisor is John Clements: aoeuclements@brinckerhoff.org
+ */
+package Models;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ *
+ * @author Bryan McGuffin
+ * @version Apr 17, 2018
+ */
+public class XML_ParseHandler extends DefaultHandler
+{
+    public XML_ParseHandler()
+    {
+        System.out.println("Constructor for handler");
+    }
+    
+    @Override
+    public void startElement (String uri, String localName, String qName, Attributes attributes)
+    {
+        System.out.println("Start: " + qName);
+    }
+    
+    @Override
+    public void endElement (String uri, String localName, String qName) throws SAXException
+    {
+        System.out.println("End: " + qName);
+    }
+    
+    @Override
+    public void characters (char ch[], int start, int length) throws SAXException
+    {
+        System.out.println("Characters: " + ch);
+    }
+    
+    public Script generateScript()
+    {
+        Script newScript = new Script();
+        newScript.addPlotline("Hello world", 0);
+        return newScript;
+    }
+}
